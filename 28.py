98aa4698692907e091196ccda5d40ad6a81c0590
sgwyrk17 <y.sagawa.0517@gmai.com>
2016-06-27 11:31:15 +0900

    made 28.py

---
+++ 28.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+
+import re
+
+def cleaner(str):
+    str = re.sub(r"(\'\'\'\'\')|(\'\'\')|(\'\')|", "", str)
+    str = re.sub(r"\[\[([^|\]]+?\|)*(.+?)\]\]", r"\2", str)
+    str = re.sub(r"\[.*?\]", r"", str)
+    str = re.sub(r"<.*?>", r"", str)
+    str = re.sub(r"\{\{.+?\|.+?\|(.+?)\}\}", r"\1", str)
+    return str
+
+with open("uk.txt") as f:
+    lines = f.readlines() #list
+    str_lines = '' #list to str
+    for line in lines:
+        str_lines += str(line)
+    str_lines = re.split(r"\n[\|}]", str_lines)
+    basicinfo_dict={}
+    for line in str_lines:
+        basicinfo_line = re.search("(.*?)\s=\s(.*)", line, re.S) #re.S = "." de \n mo match
+        if basicinfo_line is not None:
+            basicinfo_dict[basicinfo_line.group(1)] = cleaner(basicinfo_line.group(2))
+
+    for k, v in sorted(basicinfo_dict.items(), key=lambda x: x[1]):
+        print k, v
